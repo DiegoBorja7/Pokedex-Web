5a79f28c7a06b363bcf13545306bca6fc0236996
Diego <daborjas@uce.edu.ec>
2025-02-11 08:34:28 -0500

    Create Spring Boot Project

---
+++ src/main/java/ec/edu/uce/pokedex/PokedexWebApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/edu/uce/pokedex/PokedexWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
